218679490bfd1cc10be3812c020e0a4448e066f1
Rigel-R <zangeneh.iauh@gmail.com>
2026-09-06 20:14:55 +0330

    Add package metadata and project constants

---
+++ src/identity_invariant_fas/__init__.py
@@ -0,0 +1,3 @@
+"""IdentityInvariant-FAS research package."""
+
+__version__ = "0.1.0"
---
+++ src/identity_invariant_fas/constants.py
@@ -0,0 +1,5 @@
+"""Shared label conventions used throughout the project."""
+
+BONA_FIDE_LABEL = 0
+ATTACK_LABEL = 1
+NUM_FAS_CLASSES = 2
